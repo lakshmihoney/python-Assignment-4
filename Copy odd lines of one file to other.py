7cb1d3b5c15df9ccaf7a96b378a101bd95245d65
lakshmi honey <lakshmihoney2203@gmail.com>
2022-04-28 07:29:43 +0530

    assignment-4

---
+++ Copy odd lines of one file to other.py
@@ -0,0 +1,30 @@
+# open file in read mode
+fn = open('new 2.txt', 'r')
+
+# open other file in write mode
+fn1 = open('lakshmi.txt', 'w')
+
+# read the content of the file line by line
+cont = fn.readlines()
+type(cont)
+for i in range(0, len(cont)):
+	if(i % 2 ! = 0):
+		fn1.write(cont[i])
+	else:
+		pass
+
+# close the file
+fn1.close()
+
+# open file in read mode
+fn1 = open('new 2.txt', 'r')
+
+# read the content of the file
+cont1 = fn1.read()
+
+# print the content of the file
+print(cont1)
+
+# close all files
+fn.close()
+fn1.close()
---
+++ python program to print word by word in a file.py
@@ -0,0 +1,15 @@
+
+
+# file word by word
+   
+# opening the text file
+with open('new 2.txt','r') as file:
+   
+    # reading each line    
+    for line in file:
+   
+        # reading each word        
+        for word in line.split():
+   
+            # displaying the words           
+            print(word) 
